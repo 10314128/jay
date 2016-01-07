a7f9ff2542c7e5b181708e533c31ba99e41385b7
m516 <m516@m516>
2016-01-08 03:16:51 +0800

    1/7

--- jay/album/urls.py
+++ jay/album/urls.py
@@ -3,5 +3,8 @@ from album import views
 
 urlpatterns = [
     url(r'^$', views.album, name='album'),
-    
+    url(r'^category/(?P<categoryName>[\w\-]+)/$', views.category, name='category'),
+    url(r'^addCategory/$', views.addCategory, name='addCategory'),
+    url(r'^addPage/(?P<categoryName>[\w\-]+)/$', views.addPage, name='addPage'),
+    url(r'^deleteCategory/(?P<categoryID>[0-9]+)/$', views.deleteCategory,name='deleteCategory'),
 ]
--- jay/album/views.py
+++ jay/album/views.py
@@ -1,7 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from album.models import Category, Page
+from album.forms import CategoryForm, PageForm
 
 def album(request):
-    return render(request, 'album/album.html')
+    categories = Category.objects.order_by('-likes')
+    context = {'categories':categories}
+    return render(request, 'album/album.html',context)
 
+def category(request, categoryName):
+    context = {}
+    try:
+        category = Category.objects.get(name=categoryName)
+        context['category'] = category
+        context['pages'] = Page.objects.filter(category=category)
+    except Category.DoesNotExist:
+        pass
+    return render(request, 'album/category.html', context)
 
+def addCategory(request):
+    template = 'album/addCategory.html'
+    if request.method=='GET':
+        return render(request, template, {'form':CategoryForm()})
+    # request.method=='POST'
+    form = CategoryForm(request.POST)
+    if not form.is_valid():
+        return render(request, template, {'form':form})
+    form.save()
+    return redirect(reverse('album:album'))
+    # Or try this: return album(request)
 
+def addPage(request, categoryName):
+    template = 'album/addPage.html'
+    try:
+        pageCategory = Category.objects.get(name=categoryName)
+    except Category.DoesNotExist:
+        return category(request, categoryName)
+    context = {'category':pageCategory}
+    if request.method=='GET':
+        context['form'] = PageForm()
+        return render(request, template, context)
+    # request.method=='POST'
+    form = PageForm(request.POST)
+    context['form'] = form
+    if not form.is_valid():
+        return render(request, template, context)
+    page = form.save(commit=False)
+    page.category = pageCategory
+    page.save()
+    return redirect(reverse('album:category', args=(categoryName, )))
+
+def deleteCategory(request, categoryID):
+    if request.method!='POST':
+        return album(request)
+    # request.method=='POST':
+    categoryToDelete = Category.objects.get(id=categoryID)
+    if categoryToDelete:
+        categoryToDelete.delete()
+    return redirect(reverse('album:album'))
+    
